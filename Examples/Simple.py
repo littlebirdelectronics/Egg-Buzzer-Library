db36f6f90ce19ec17c05b0e28a16a67f08b0612d
LH <vk2gzz@ieee.org>
2021-02-18 10:00:26 +1100

    typo

--- Examples/Simple.py
+++ Examples/Simple.py
@@ -4,7 +4,7 @@ import time
 buzzer = Passive_Buzzer.Buzzer(13)
 
 buzzer.Start(1000, 50)
-time.sleep(1000)
+time.sleep(1)
 buzzer.ChangeFrequency(1500)
-time.sleep(1000)
+time.sleep(1)
 buzzer.Stop();
